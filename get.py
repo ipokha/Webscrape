97fd570bb3be26018d4204fca6a3cbbfd71ffe47
Isit Pokharel <isitpokharel96@gmail.com>
2021-04-17 14:23:05 -0400

    new changes

--- get.py
+++ get.py
@@ -6,14 +6,20 @@ import argparse
 import csv
 
 def get_page(url,params=None):
+    """returns the content of a given url"""
     page = session.get(url,params=params,verify=False).content
     return page
 
 def get_bsobj(page):
+    """Returns a beautiful soup object of a given page"""
     bsobj = soup(page,"lxml")
     return bsobj
 
 def get_divs(bsobj):
+    """All information related to a single event in the page is
+    contained in a div tage with class name "list-grid-item. Get all
+    the div tags with this class name in a page and returns it"""
+
     all_divs = bsobj.find_all("div",attrs={"class":"list-grid-item"})
     if all_divs != []:
         return all_divs
@@ -21,6 +27,9 @@ def get_divs(bsobj):
         return None
 
 def get_event_title(div):
+    """Event title name and event url is contained in h3 tag with class name that
+    contains "list-title. Find that tag and return event title and even name"""
+
     title_pattern = re.compile("list-title")
     title = div.find("h3",attrs={"class",title_pattern})
     event_title = None
@@ -37,6 +46,11 @@ def get_event_title(div):
     return event_title,event_url
 
 def get_event_place(div):
+    """name of the place, and website url of the place is contained in p tag
+    with class name that contains serif in it, get name of place and url of place
+    website from this tag and return it"""
+
+    
     place_pattern = re.compile("serif")
     place = div.find("p",attrs={"class",place_pattern})
     place_name = None
@@ -53,6 +67,9 @@ def get_event_place(div):
     return place_name, place_url
 
 def get_event_location(div):
+    """Location name is contained in p tag with class name called styled, return
+    text in that p tag"""
+
     location = div.find("p", attrs = {"class":"styled"})
     location_name = None
     try:
@@ -62,6 +79,10 @@ def get_event_location(div):
     return location_name
 
 def get_event_date(div):
+
+    """event date is contained in div tag with class name list-info,
+    return the text in that div tag"""
+
     date = div.find("div", attrs = {"class":"list-info"})
     date_text = None
     try:
@@ -71,6 +92,9 @@ def get_event_date(div):
     return date_text
 
 def get_event_time(div):
+    """event time is present in div tag with class name "list-info mb-2,
+    return text in that div tag
+    """
     time  = div.find("div", attrs = {"class":"list-info mb-2"})
     time_text = None
     try:
@@ -80,22 +104,29 @@ def get_event_time(div):
     return time_text
 
 def collect_info(div):
+    """Collect all the required information from the given div tag with class name
+    list-grid-item """
     title,event_url = get_event_title(div)
     place_name,place_url = get_event_place(div)
     place_address   = get_event_location(div)
     event_date = get_event_date(div)
     event_time =  get_event_time(div)
     info_list = [title,event_url,place_name,place_url,place_address,event_date,event_time]
+    # if all items are NONE return None, else return info_list
     if all(item is None for item in info_list):
-        pass
+        return None
     else:
         return info_list
 
 def get_str_dates(additional_days):
+    """returns two dates -start date and end dates, in str format of "mm/dd/yyyy",
+    where two days are seperated by additional_days"""
+
     today = datetime.datetime.today()
     end_date = today+datetime.timedelta(days=additional_days)
     today_str = today.strftime("%m/%d/%Y")
     end_date_str = end_date.strftime("%m/%d/%Y")
+
     return today_str, end_date_str
 
 def write_to_csv(event_list, output_name = "output.csv"):
@@ -114,9 +145,15 @@ def main():
 
 
     all_events = []
+
+    #start a requests session
     session = requests.session()
+
+    #get beautiful soup object of the page
     bsobj = get_bsobj(session.get(url).content)
-    pages_to_navigate = 20
+
+    #number of pages to navigate in the website
+    pages_to_navigate = 10
 
     for page_num in range(pages_to_navigate):
         values = {
